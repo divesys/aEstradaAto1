3ea72dd72d980858861506887f1d5b21501298e4
David Paiva <draxdeveloper@gmail.com>
2017-01-19 15:55:47 -0200

    esqueci desse arquivo

---
+++ interface/HUD/exibeContadorDeRealizacao.gd
@@ -0,0 +1,38 @@
+ #determina quando o icone devera aparecer
+
+extends Control
+
+onready var parteAtual = get_node("Sprite")
+onready var indiceAtual = 0
+onready var tocouAnimacao = false
+onready var icone = get_node("Sprite") #o nó do icone
+onready var labelValor = get_node("valor") #o nó da label com o valor
+onready var brilho = get_node("brilhoSurgir")
+onready var anim = get_node("anim")
+
+func _ready():
+	
+	set_process(true)
+		
+	#torna o elemento da interface invisivel
+	#controleFluxoHistoria.pularIndiceParte(
+	icone.set_opacity(0)
+	labelValor.set_opacity(0)
+	brilho.set_emitting(false)
+	
+func _process(delta):
+	
+	parteAtual = controleFluxoHistoria.getParte()
+	indiceAtual = controleFluxoHistoria.getIndiceParte()
+	if(parteAtual == "prologo" and indiceAtual == 23):
+		
+		if(tocouAnimacao == false):
+			
+			anim.play("aparecer")
+			tocouAnimacao = true
+			
+		if(!anim.is_playing()):
+			
+			controleFluxoHistoria.acrescentaIndiceParte(get_name()) 
+			
+	labelValor.set_text(str(globais.getEnergiaRealizacaoSuposta()))
